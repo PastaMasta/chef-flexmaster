11179c033286bd8027cc9b1e71c9360660a2fd71
PastaMasta <pasta.masta2902@gmail.com>
2015-12-17 11:00:19 +0000

    Re-orderd service starting (rpcbind needs to be befofe nfs)

--- recipes/fileshare.rb
+++ recipes/fileshare.rb
@@ -13,21 +13,25 @@
   end
 end
 
+['rpcbind','nfs'].each do |service|
+  service service do
+    action [ :enable, :start ]
+  end
+end
+
 template "/etc/exports" do
   source "exports.erb"
   mode 0644
   notifies :restart, 'service[nfs]', :immediately
 end
 
-['nfs'].each do |service|
-  service service do
-    action [ :enable, :start ]
-  end
-end
-
 # http
 package 'httpd' do
   action :install
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
 end
 
 template "/etc/httpd/conf.d/#{node['repo']['name']}.conf" do
@@ -36,13 +40,13 @@ template "/etc/httpd/conf.d/#{node['repo']['name']}.conf" do
   notifies :restart, 'service[httpd]', :immediately
 end
 
-service 'httpd' do
-  action [ :enable, :start ]
-end
-
 # Samba
 package 'samba' do
   action :install
+end
+
+service 'smb' do
+  action [ :enable, :start ]
 end
 
 template '/etc/samba/smb.conf' do
@@ -55,8 +59,4 @@ template '/etc/samba/smbusers' do
   source "smbusers.erb"
   mode 0644
   notifies :restart, 'service[smb]', :immediately
-end
-
-service 'smb' do
-  action [ :enable, :start ]
 end
