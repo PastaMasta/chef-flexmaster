7176450ac667396eed458633e7fbd31b110d3611
PastaMasta <pasta.masta2902@gmail.com>
2016-01-25 10:05:56 +0000

    Moved pxeboot into buildserver

--- recipes/buildserver.rb
+++ recipes/buildserver.rb
@@ -6,15 +6,18 @@
 # Copyright 2015, PastaMasta
 #
 
+# Setup kickstarts
 basedir = node['buildserver']['basedir']
 
 directory basedir do
   action :create
+  recursive true
 end
 
 node['buildserver']['dirs'].each do |dir|
   directory File.join(basedir,dir) do
     action :create
+    recursive true
   end
 end
 
@@ -27,4 +30,28 @@ end
 git File.join(basedir,'kickstarts') do
   repository node['buildserver']['kickstarts']['repo']
   action :sync
+end
+
+# Setup pxeboot
+
+['xinetd','tftp-server'].each do |package|
+  package package do
+    action :install
+  end
+end
+
+service 'xinetd' do
+  action [ :enable, :start ]
+end
+
+directory node['pxeboot']['tftpboot'] do
+  action :create
+end
+
+template "/etc/xinetd.d/tftp" do
+  source "tftp.erb"
+  owner 'root'
+  group 'root'
+  mode 0600
+  notifies :reload, 'service[xinetd]', :immediately
 end
