3ee6d3569497fc4c656bb465ab8961ef65cff754
PastaMasta <pasta.masta2902@gmail.com>
2016-11-16 21:09:53 +0000

    Initial pxe setup

--- recipes/buildserver.rb
+++ recipes/buildserver.rb
@@ -13,3 +13,17 @@ git "#{node['repo']['root']}/repo/build/kickstarts" do
 end
 
 # Setup tftp / pxeboot
+package 'tftp-server' do
+  action :install
+end
+
+# Setup pxe menus
+directory '/var/lib/tftpboot/pxelinux.cfg' do
+  action :create
+end
+[ 'default', 'diag', 'installers', 'other' ].each do |menu|
+  template "/var/lib/tftpboot/pxelinux.cfg/#{menu}" do
+    source 'pxe/' + menu + '.erb'
+    action :create
+  end
+end
