26f34ad4e495c66f782cd990461d08b39dd92503
PastaMasta <pasta.masta2902@gmail.com>
2016-11-16 22:35:21 +0000

    Added image copying for pxeboots

--- recipes/buildserver.rb
+++ recipes/buildserver.rb
@@ -31,12 +31,10 @@ end
   'installers',
   'other'
 ].each do |menu|
-
   template "/var/lib/tftpboot/pxelinux.cfg/#{menu}" do
     source 'pxe/' + menu + '.erb'
     action :create
   end
-
 end
 
 # Setup pxe menu bootimages
@@ -50,10 +48,39 @@ end
   'menu.c32',
   'pxelinux.0'
 ].each do |file|
-
   remote_file File.join('/var/lib/tftpboot/',file) do
     source 'file://' + File.join('/usr/share/syslinux/',file)
     action :create
   end
+end
 
+# Setup image dirs
+[ 'diag',
+  'installers',
+  'installers/centos6-x86_64',
+  'other'
+].each do |dir|
+  directory File.join('/var/lib/tftpboot/bootimages/',dir) do
+    recursive true
+    action :create
+  end
+end
+
+# Copy in images from mrepo if they exist (mrepo may not have run yet)
+[
+
+  { 'source' => '/data/repo/mrepo/centos6-x86_64/os/isolinux/memtest',
+    'target' => '/var/lib/tftpboot/bootimages/diag/memtest' },
+
+  { 'source' => '/data/repo/mrepo/centos6-x86_64/os/images/pxeboot/vmlinuz',
+    'target' => '/var/lib/tftpboot/bootimages/installers/centos6-x86_64/vmlinuz' },
+
+  { 'source' => '/data/repo/mrepo/centos6-x86_64/os/images/pxeboot/initrd.img',
+    'target' => '/var/lib/tftpboot/bootimages/installers/centos6-x86_64/initrd.img' }
+
+].each do |bootimage|
+  remote_file bootimage['target'] do
+    source 'file://' + bootimage['source']
+    action :create
+  end if File.readable?(bootimage['source'])
 end
