cf6ed20bbdf62ac80a89dc09ffe75f26363e1c68
PastaMasta <pasta.masta2902@gmail.com>
2016-11-16 21:52:09 +0000

    Added pxe boot image copying

--- recipes/buildserver.rb
+++ recipes/buildserver.rb
@@ -17,13 +17,43 @@ package 'tftp-server' do
   action :install
 end
 
+service 'tftp' do
+  action :enable
+end
+
 # Setup pxe menus
 directory '/var/lib/tftpboot/pxelinux.cfg' do
   action :create
 end
-[ 'default', 'diag', 'installers', 'other' ].each do |menu|
+
+[ 'default',
+  'diag',
+  'installers',
+  'other'
+].each do |menu|
+
   template "/var/lib/tftpboot/pxelinux.cfg/#{menu}" do
     source 'pxe/' + menu + '.erb'
     action :create
   end
+
+end
+
+# Setup pxe menu bootimages
+package 'syslinux' do
+  action :install
+end
+
+[ 'chain.c32',
+  'mboot.c32',
+  'memdisk',
+  'menu.c32',
+  'pxelinux.0'
+].each do |file|
+
+  remote_file File.join('/var/lib/tftpboot/',file) do
+    source 'file://' + File.join('/usr/share/syslinux/',file)
+    action :create
+  end
+
 end
