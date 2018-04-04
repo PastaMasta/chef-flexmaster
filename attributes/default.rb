a29346bfec97b950a1b1b8ade61a2620ea2bd777
PastaMasta <pasta.masta2902@gmail.com>
2018-04-04 22:04:04 +0100

    attributes movearound

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,7 +6,6 @@
 # Copyright 2017, PastaMasta
 #
 
-
 # Layout basics
 override['data']['root'] = '/data'
 override['data']['layout'] = [
@@ -19,42 +18,24 @@ override['data']['layout'] = [
   'repo/media',
   'repo/media/movies',
   'repo/media/music',
-  'repo/media/picture',
+  'repo/media/pictures',
+  'repo/media/memories',
   'repo/media/shows',
   'repo/os',
   'repo/os/isos',
   'repo/software',
   'users'
 ]
+
+
+default['base']['users'] = {
+  'repo' => { 'uid'=>2000,'home'=>'/data/repo','shell'=>'/sbin/nologin' }
+}
+default['base']['groups'] = {
+  'docs' => { 'gid'=>3000,'members'=> [] }
+}
+
 default['repo']['repo_user'] = 'repo'
 default['repo']['docs_user'] = 'sbt'
 
-# Build server kickstarts
-default['repo']['pxebuild']['git'] = 'https://github.com/PastaMasta/pxe-builds.git'
-# tftp bootimages
-default['repo']['bootimage_dirs'] = %w(
-  diag
-  installers
-  installers/centos7-x86_64
-  installers/centos6-x86_64
-  other
-)
-default['repo']['bootimages'] = [
-  { 'source' => "#{node['data']['root']}/repo/os/CentOS/6/os/x86_64/isolinux/memtest",
-    'target' => 'diag/memtest' },
-
-  { 'source' => "#{node['data']['root']}/repo/os/Other/dban.bzi",
-    'target' => 'diag/dban.bzi' },
-
-  { 'source' => "#{node['data']['root']}/repo/os/CentOS/6/os/x86_64/images/pxeboot/vmlinuz",
-    'target' => 'installers/centos6-x86_64/vmlinuz' },
-
-  { 'source' => "#{node['data']['root']}/repo/os/CentOS/6/os/x86_64/images/pxeboot/initrd.img",
-    'target' => 'installers/centos6-x86_64/initrd.img' },
-
-  { 'source' => "#{node['data']['root']}/repo/os/CentOS/7/os/x86_64/images/pxeboot/vmlinuz",
-    'target' => 'installers/centos7-x86_64/vmlinuz' },
-
-  { 'source' => "#{node['data']['root']}/repo/os/CentOS/7/os/x86_64/images/pxeboot/initrd.img",
-    'target' => 'installers/centos7-x86_64/initrd.img' }
-]
+default['repo']['chef-options']['dir'] = '/root/chef-options'
