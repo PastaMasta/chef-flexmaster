f46a4d2fd8a776ea31d2684e9949e879692a6189
PastaMasta <pasta.masta2902@gmail.com>
2018-04-04 21:19:40 +0100

    Added file-option grabber

---
+++ recipes/options.rb
@@ -0,0 +1,28 @@
+#
+# Author:: PastaMasta (<pasta.masta2902@gmail.com>)
+# Cookbook Name:: chef-master
+# Recipe:: options
+#
+# Copyright 2017, PastaMasta
+#
+
+# Deploys files and re-reads contents into node data for configuring the Chef
+# run from files on disk
+options_dir = node['repo']['chef-options']['dir']
+directory options_dir
+
+%w(
+  backups
+  desktops
+).each do |file|
+
+  filepath = File.join(options_dir,file)
+
+  template filepath do
+    source File.join('options',"#{file}.erb")
+    action :create_if_missing
+  end
+
+  node.default['repo']['chef-options'][file] = File.readlines(filepath).reject {|a| a.match(/^#/)}.map(&:strip)
+
+end
