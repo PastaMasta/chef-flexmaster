d70dfc1d481a18da8a98d5365c85764de9a0bd76
PastaMasta <pasta.masta2902@gmail.com>
2020-07-18 17:24:02 +0100

    version bumpy

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'Stephen@tandy-uk.net'
 license          'All rights reserved'
 description      'Standard lab utility server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.4.0'
 
 depends 'chef-base'
